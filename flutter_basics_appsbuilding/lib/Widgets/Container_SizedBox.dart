6b83834f1d0b755a93788847cf573e682ef61647
Devan <devanshsadhya77@gmail.com>
2024-10-20 14:27:32 +0530

    1st Container Sized Box widgets

--- flutter_basics_appsbuilding/lib/Widgets/Container_SizedBox.dart
+++ flutter_basics_appsbuilding/lib/Widgets/Container_SizedBox.dart
@@ -7,9 +7,16 @@ class Container_SizedBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Basics'),
+        title: Text('Container and SizedBox '),
         backgroundColor:Colors.deepPurple,
       ),
+      body:Center(
+        child: SizedBox(
+          height: 650,
+          width: 650,
+          child: Center(child: Text('This is sized Box ')),
+        ),
+      ) ,
     );
   }
 }

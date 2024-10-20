4384be84817eacc470dbfceb687fb62d01693afb
Devan <devanshsadhya77@gmail.com>
2024-10-20 11:40:47 +0530

    1st Container Sized Box widgets

---
+++ flutter_basics_appsbuilding/lib/Widgets/Container_SizedBox.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Container_SizedBox extends StatelessWidget {
+  const Container_SizedBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Basics'),
+        backgroundColor:Colors.deepPurple,
+      ),
+    );
+  }
+}
+
---
+++ flutter_basics_appsbuilding/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_basics_appsbuilding/Widgets/Container_SizedBox.dart';
+
+void main(){
+  runApp(new MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.deepPurple,
+      ),
+      debugShowCheckedModeBanner: false,
+      home:Container_SizedBox(),
+    );
+  }
+}

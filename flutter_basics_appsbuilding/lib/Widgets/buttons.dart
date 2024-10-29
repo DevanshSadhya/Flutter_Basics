e3894bdcc6503441665577ac02ee9f56079fb8ae
Devan <devanshsadhya77@gmail.com>
2024-10-29 10:17:14 +0530

    3rd buttons  widgets

--- flutter_basics_appsbuilding/lib/Widgets/buttons.dart
+++ flutter_basics_appsbuilding/lib/Widgets/buttons.dart
@@ -15,13 +15,45 @@ class ButtonWidget extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Center(
+
+            // TextButtons
+
             child: TextButton(
-              onPressed: (){},
-              child: Text("Press Me"),
+              onPressed: (){
+                print("TextButton"); //printing the output in the console
+              },
+              child: Text("Press Me",style: TextStyle(
+                fontSize: 25,
+                color: Colors.black
+              ),),
               style: ButtonStyle(
+                // padding: WidgetStatePropertyAll(EdgeInsets.all(50)),
                 overlayColor: WidgetStatePropertyAll(Colors.red),
                 backgroundColor: WidgetStatePropertyAll(Colors.yellow)
               ),
+            ),
+          ),
+
+          SizedBox(height: 60),
+
+          //Elevated Buttons
+
+          Container(
+            // height: 70,
+            // width: 200,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.red),
+                overlayColor: WidgetStatePropertyAll(Colors.blue),
+              ),
+              onPressed: (){
+                print("Elevated Button"); //printing the output in console
+              },
+              child: Text("Elevated Buttons",style: TextStyle(
+                fontSize: 25,
+                color: Colors.yellow,
+
+              ),),
             ),
           )
         ],
